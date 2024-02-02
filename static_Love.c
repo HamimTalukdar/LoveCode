51a9614a992523d71822828c60dc9d3f600d3826
Hamim <t.i.m.hamim@gmail.com>
2024-02-02 18:36:01 +0600

    staticLove.c file added

---
+++ static_Love.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+  int i,j,k;
+  for(i=0,k=0;i<13;i++,k=k+2){
+    if(i==5){ 
+      k=k-1;
+    }
+    for(j=0;j<31;j++){
+        if(i<3 && ((j>=3-k && j<=9+k) || (j>=21-k && j<=27+k))){
+          printf("*");
+        }
+        else if(i>2 && (j>=k-7 && j<=37-k) || (i==12 && j==15 )){
+          printf("*");
+        }
+        else 
+        printf(" ");
+    }
+    printf("\n");
+  }
+  return 0;  
+}
